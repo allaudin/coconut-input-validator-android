360c355a722967150c09d787023629fb39984bea
anullbyte@gmail.com <anullbyte@gmail.com>
2018-07-01 18:34:04 -0400

    Refactor test app, update dependencies

--- app/src/main/java/io/github/allaudin/demo/MainActivity.java
+++ app/src/main/java/io/github/allaudin/demo/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.allaudin.demo;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import io.github.allaudin.coconut.CoconutValidator;
+import io.github.allaudin.demo.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,14 +16,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final ViewGroup root = findViewById(R.id.content);
-        findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
+        final ActivityMainBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean areFieldsValid = CoconutValidator.validateLayout(root);
+                boolean areFieldsValid = CoconutValidator.validateLayout(binding.getRoot());
                 if (areFieldsValid) {
-                    Snackbar.make(root, R.string.all_ok, Snackbar.LENGTH_SHORT)
+                    Snackbar.make(binding.getRoot(), R.string.all_ok, Snackbar.LENGTH_SHORT)
                             .show();
                 }
             }
