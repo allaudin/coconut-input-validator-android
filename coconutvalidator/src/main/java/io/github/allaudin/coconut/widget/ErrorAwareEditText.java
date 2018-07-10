10cad393c2ac007e2ba1782dc68b1ed079a41af1
anullbyte@gmail.com <anullbyte@gmail.com>
2018-07-10 00:05:23 -0400

    Refactor

--- coconutvalidator/src/main/java/io/github/allaudin/coconut/widget/ErrorAwareEditText.java
+++ coconutvalidator/src/main/java/io/github/allaudin/coconut/widget/ErrorAwareEditText.java
@@ -1,15 +1,11 @@
 package io.github.allaudin.coconut.widget;
 
-import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.util.AttributeSet;
-import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
